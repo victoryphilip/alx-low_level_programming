668d286de5f40ea76ca23b291d345e55a79a4b52
victoryphilip <victorymartin22@gmail.com>
2022-09-08 16:56:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Akways 0 (sucess)
+ */
+int main(void)
+{
+	write(2, "and that place odf art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
