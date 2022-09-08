8a121ca7fed0de879ce37da24e244dfe06b1223e
victoryphilip <victorymartin22@gmail.com>
2022-09-08 13:08:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ * main - entry point 
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
