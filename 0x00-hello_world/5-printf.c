ef5e0130f4e6f93ef9950e452b2ae2c19de9ed2d
victoryphilip <victorymartin22@gmail.com>
2022-09-08 13:47:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include ‹stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 after printing the function
+*/
+int main(void)
+{puts ("\ "Programming is like building a multilingual puzzle");
+	return (0);
+} 
