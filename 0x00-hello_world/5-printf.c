22ffb80855643e409ccffb3fb058cd36f42728cc
victoryphilip <victorymartin22@gmail.com>
2022-09-08 14:45:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,23 +3,15 @@
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		
-		return (0)
-			;
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
