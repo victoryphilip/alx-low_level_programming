3a522dad7fc3d445f7e45971d953a6b2b2d9e902
victoryphilip <victorymartin22@gmail.com>
2022-09-09 11:03:38 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main Entry point
+*
+*Return: Always @ (Seccess/correct)
+*/
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet =	'a'; alphabet <= '2'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
