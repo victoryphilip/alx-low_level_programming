8ccbfd3e6d44a2f96d57d76c19ac7bd0dd6bd92e
victoryphilip <victorymartin22@gmail.com>
2022-09-09 16:15:49 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include stdio.h>
+
+/**
+ * main - output all single digit number of base 10
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 9; var++)
+	{
+		int ii;
+
+		for (ii = var + 1; ii < 10; ii++)
+		{
+			putchar((var % 10) + '0');
+			putchar((ii % 10) + '0');
+
+			if (var != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+
+	}
+	putchar('\n');
+	return (0);
