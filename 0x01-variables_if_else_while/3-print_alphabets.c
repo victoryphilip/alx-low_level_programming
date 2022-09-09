74183af89b21c637bd25ad551841f9026409885f
victoryphilip <victorymartin22@gmail.com>
2022-09-09 14:23:06 -0700

    done3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,16 @@ char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-Putchar(lower_case);
+putchar(lower_case);
 }
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
-Putchar(upper_case);
+putchar(upper_case);
 }
 
 
-Putchar('\n');
+putchar('\n');
 
 return (0);
 
