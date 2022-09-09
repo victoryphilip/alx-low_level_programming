74a237a70ae891a7e8c15f8b04b6b80940044ff0
victoryphilip <victorymartin22@gmail.com>
2022-09-08 17:00:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Akways 0 (sucess)
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
 	write(2, "and that place odf art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
+}
