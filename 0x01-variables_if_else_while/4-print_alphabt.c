829bcfe7ebcb013af47a60e72e4da274c68fcdda
victoryphilip <victorymartin22@gmail.com>
2022-09-09 15:08:12 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,14 @@ int main(void)
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	if (alphabet == 'q')
-		continue;
+{	
+if (alphabet == 'q')
+continue;
 
-	else if (alphabet == 'e')
-		continue;
+else if (alphabet == 'e')
+continue;
 
-        putchar (salphabet);
+putchar (salphabet);
 }
 
 putchar ('\n');
