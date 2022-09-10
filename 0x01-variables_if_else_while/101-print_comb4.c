db2293c98414ad350503aec308919f140224b25c
victoryphilip <victorymartin22@gmail.com>
2022-09-09 17:15:19 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - program to print all possible combinations of three digits
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 8; var++)
+	{
+		int ii;
+
+		for (ii = var + 1; ii < 9; ii++)
+		{
+			int jj;
+
+			
+	                for (jj = ii + 1; jj < 10; jj++)
+                         {
+				 putchar((var % 10) + '0');
+				 putchar((ii % 10) + '0');
+				 putchar((jj % 10) + '0');
+
+				 if (var != 7)
+				 {
+					 putchar(',');
+					 putchar(' ');
+				 } else
+					 continue;
+			 }
+		}
+         }
+         
+         putchar('\n');
+	 return (0);
+}
