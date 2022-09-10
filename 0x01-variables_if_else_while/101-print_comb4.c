e9d4f1eaa4aa9779de3dc3b9261de8a2c8180ace
victoryphilip <victorymartin22@gmail.com>
2022-09-09 17:31:47 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,26 @@
  */
 int main(void)
 {
-	int var;
+	int var; 
 
 	for (var = 0; var < 8; var++)
 	{
-		int ii;
+		int ii; 
 
 		for (ii = var + 1; ii < 9; ii++)
 		{
 			int jj;
 
-			
 	                for (jj = ii + 1; jj < 10; jj++)
                          {
 				 putchar((var % 10) + '0');
 				 putchar((ii % 10) + '0');
-				 putchar((jj % 10) + '0');
+				 putchar((jj % 10) + '0'); 
 
 				 if (var != 7)
 				 {
 					 putchar(',');
-					 putchar(' ');
+					 putchar(' '); 
 				 } else
 					 continue;
 			 }
