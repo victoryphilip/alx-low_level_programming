0e071c0ffe137a44e5f3a3ac78ef7d9061ce0176
victoryphilip <victorymartin22@gmail.com>
2022-09-13 11:55:52 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+
+		-putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,6 @@
+#include "main.h"
 /**
 * print_alphabet - function to print abc
 *
-* Return:0
+* Return: 0 
 */
-void print_alphabet(void)
-{
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
-	}
-		_putchar(c);
-	}
-_putchar('\n');
-}
-^c
-
