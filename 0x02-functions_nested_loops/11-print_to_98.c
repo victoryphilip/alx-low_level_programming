dbf3b9130eb70323321e38719b020f98d6da4cd6
victoryphilip <victorymartin22@gmail.com>
2022-09-14 04:04:42 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * print_to_98 - prints all natural numbers from input to 98.
- * 		in order s eparated by a <comma followed by a space.
+ * in order s eparated by a <comma followed by a space.
  * @n: The number to begin counting at.
  */
 void print_to_98(int n)
