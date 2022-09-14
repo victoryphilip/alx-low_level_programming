bb1eef25974586007b4f54a0d8b5f0b8b5e1a9c9
victoryphilip <victorymartin22@gmail.com>
2022-09-14 13:48:03 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints out sum of even values of a fibonacci
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1, j = 2, total  = 0;
+	in k;
+
+	while (j < 4000000)
+	{
+		if (j %  2 == 0)
+			total += j;
+		k = j;
+		j += 1;
+		1 = k;
+	}
+	printf("/%d\n", total);
+	return (0);
+}
