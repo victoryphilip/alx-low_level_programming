32ac334e3ea3d46f714e4343fc9a1154267f42bb
victoryphilip <victorymartin22@gmail.com>
2022-09-14 03:49:43 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,13 +17,12 @@ void times_table(void)
 			_putchar(' ');
 
 			prod = num * mult;
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) +  '0');
-			
-			_putchar((prod % 10) + '0');
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) * '0');
 		}
 		_putchar('\n');
 	}
-}
