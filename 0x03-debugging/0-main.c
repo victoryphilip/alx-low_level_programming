1bbd21dec75ad57f256056fcd1a3357f7d6d94ce
victoryphilip <victorymartin22@gmail.com>
2022-09-15 10:46:22 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-/**
- * main - tests function that prints if integer is positive or negative 
+/**main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
