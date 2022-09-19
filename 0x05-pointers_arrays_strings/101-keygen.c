75c220e9a959c70faada64d95f2d4470ac5eb860
victoryphilip <victorymartin22@gmail.com>
2022-09-19 14:34:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -33,6 +33,14 @@ int main(void)
 
 		for (index = 0; password[index]; index++)
 		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
 			if (password[index] >= (33 + diff_half2))
 			{
 				password[index] -= diff_half2;
