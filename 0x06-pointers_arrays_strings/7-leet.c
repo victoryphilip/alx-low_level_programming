6a0de951dcbea091ca219fddbf8f4d2d56886c63
victoryphilip <victorymartin22@gmail.com>
2022-09-21 12:32:11 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,17 @@
  */
 char *leet(char *n)
 {
-	int i x;
-	int find = {'a', 'A', 'e', 'E', 'o' O', 't', 'T', '1', 'L'};
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o' 'O', 't', 'T', '1', 'L'};
 	int replacer[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (x  = 0; x  <= 9; x++)
+		for (x  = 0; x <= 9; x++)
 		{
 			if (n[i] == find[x])
 			{
-				n[i] = replacer[x /2];
+				n[i] = replacer[x / 2];
 				x = 9;
 			}
 		}
