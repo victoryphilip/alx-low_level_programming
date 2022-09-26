2f10fd5e4b9b448bc29a4c8489b88a173c634de5
victoryphilip <victorymartin22@gmail.com>
2022-09-26 12:19:15 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 	int i, n, totall = 0, total2 = 0;
 
 	for (i = 0; i <= (size * size); i = i + size + 1)
-		total = totall + a[i];
+		totall = totall + a[i];
 
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 		total2 = total2 + a[n];
