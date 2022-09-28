433b6a3325cc9f9242ff9053ccd5ccea0fa0f43a
victoryphilip <victorymartin22@gmail.com>
2022-09-28 13:13:35 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@ int squareroot(int n, int i);
 /**
  * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
  * @n: input
- * Return: Always 0 (Success0
+ * Return: Always 0 (Success)
  */
 int _sqrt_recursion(int n)
 {
