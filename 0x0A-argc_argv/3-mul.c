545685917824376ba6ed01e545ad8414ac7c6e00
victoryphilip <victorymartin22@gmail.com>
2022-09-30 11:49:56 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-#include <stdlb.h>
+#include <stdlib.h>
 #include "main.h"
+
 /**
- * main - Entry point
- * atoi is a function that converts a string into an int
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if false
  */
 int main(int argc, char *argv[])
 {
-	int i, res = 1;
-
-	if (argc != 3)
-	{
-		printf("%s\n", "Error");
-		return (1);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			res *= atoi(argv[i]);
-		}
-		printf("%d\n", res);
-	}
-	return (0);
+int a, b;
+if (argc == 1)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
+}
+printf("Error\n");
+return (1);
 }
