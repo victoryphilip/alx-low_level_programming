8bd26c438652ed9c687293e3a7395b235c2be950
victoryphilip <victorymartin22@gmail.com>
2022-10-06 13:07:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ int _strlen(char *string)
 	return (i);
 }
 /**
- * string_noncat - concatenate s1 and n bytes of s2; return prt to string
+ * string_nonconcat - concatenate s1 and n bytes of s2; return prt to string
  * @s1: string 1
  * @s2: string 2
  * @n: n bytes to concat from string 2
@@ -29,7 +29,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	num = n;
 
 	if (s1 == NULL) /* account for NULL strings */
-	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
@@ -41,10 +40,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
 	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-	if (ptr ==NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++) /*concat */
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
 		ptr[i] = s1[i];
 	for (j = 0; j < num; j++)
 		ptr[i + j] = s2[j];
