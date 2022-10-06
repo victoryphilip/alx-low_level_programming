64e1269c4cdc47a29828fa9e14b28873d24c6bed
victoryphilip <victorymartin22@gmail.com>
2022-10-06 14:10:26 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
-void _putchar(char *str);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *string_nonconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsinged int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-
-#endif
+#endif /* MAIN_H_ */
