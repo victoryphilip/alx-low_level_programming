2708160e4b4814904bdf2ad688b9a935e0c13c1b
victoryphilip <victorymartin22@gmail.com>
2022-10-13 03:03:26 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index (int *, int, int (*)(int));
