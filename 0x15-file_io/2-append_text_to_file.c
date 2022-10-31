e35c6d97e1bce5e8c227935db439ff62ff40ba0f
victoryphilip <victorymartin22@gmail.com>
2022-10-31 05:41:00 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,4 +14,26 @@ int append_text_to_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
+	if (!filename)
+		return (-1);
 
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
